c4cb9ae7281bc8b31c51d6ba303b8c1ac5887d05
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-03-30 20:22:53 +0530

    added audio stegnography into main file

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request, redirect, url_for, send_file
 import pyotp
 import firebase_admin
 from firebase_admin import credentials, auth
 import base64
 import qrcode
 from io import BytesIO
+import wave
+import os
 
 app = Flask(__name__)
 
 cred = credentials.Certificate("e-authentication-using-qr-code-firebase-service-key.json")
 firebase_admin.initialize_app(cred)
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['OUTPUT_FOLDER'] = 'static/output'
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
 
 @app.route('/')
 def index():
@@ -61,7 +69,6 @@ def signin():
         totp_code = request.form.get('totp_code')
         print(totp_code)
 
-
         try:
             key = "EAuthenticationUsingQRCode"
 
@@ -76,6 +83,53 @@ def signin():
             return render_template('error.html', error=str(e))
 
     return render_template('signin.html')
+
+def hide_message(input_audio, secret_message, output_audio):
+    waveaudio = wave.open(input_audio, mode='rb')
+    frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+    secret_message = secret_message + int((len(frame_bytes)-(len(secret_message)*8*8))/8) *'#'
+    bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in secret_message])))
+    for i, bit in enumerate(bits):
+        frame_bytes[i] = (frame_bytes[i] & 254) | bit
+    frame_modified = bytes(frame_bytes)
+    with wave.open(output_audio, 'wb') as fd:
+        fd.setparams(waveaudio.getparams())
+        fd.writeframes(frame_modified)
+    waveaudio.close()
+
+@app.route('/hide', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['audio_file']
+    secret_message = request.form['secret_message']
+
+    if uploaded_file.filename != '':
+        audio_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        uploaded_file.save(audio_path)
+
+        output_audio_path = os.path.join(app.config['OUTPUT_FOLDER'], 'modified_audio.wav')
+        hide_message(audio_path, secret_message, output_audio_path)
+
+        modified_audio_path = os.path.join(app.config['OUTPUT_FOLDER'], 'modified_audio.wav')
+
+        return send_file(modified_audio_path, as_attachment=True)
+
+@app.route('/extract', methods=['POST'])
+def extract_message():
+    uploaded_file = request.files['audio_file']
+    audio_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+    uploaded_file.save(audio_path)
+
+    waveaudio = wave.open(audio_path, mode='rb')
+    frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+    extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+    string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+    msg = string.split("###")[0]
+    waveaudio.close()
+
+    if not msg or not msg.isprintable():
+        return " "
+
+    return f"{msg}"
 
 if __name__ == '__main__':
     app.run(debug=True)
